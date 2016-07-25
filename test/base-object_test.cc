953c39760048e8637161a63f3972a0d32ffb5027
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-07-25 10:25:51 +0200

    Adding NODE_WANT_INTERNALS

--- test/base-object_test.cc
+++ test/base-object_test.cc
@@ -1,4 +1,12 @@
 #include "gtest/gtest.h"
+#include "v8.h"
+
+#define NODE_WANT_INTERNALS 1
+
+#include "base-object.h"
+#include "base-object-inl.h"
 
 TEST(BaseObject, baseObject) {
+  v8::Local<v8::Object> o;
+  node::BaseObject bo {nullptr, o};
 }
