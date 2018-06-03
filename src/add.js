66f745f9edafcb4796db36b730543d469b1e66cb
Daniel Bevenius <daniel.bevenius@gmail.com>
2018-06-03 12:50:22 +0200

    src: remove log statements

--- src/add.js
+++ src/add.js
@@ -1,19 +1,13 @@
 const fs = require('fs');
 const buffer = fs.readFileSync('../scripts/helloworld.wasm');
-console.log(WebAssembly.validate(buffer));
+WebAssembly.validate(buffer);
 
-const promise = WebAssembly.instantiate(buffer, {});
-console.log(promise);
+const promise = WebAssembly.instantiate(buffer);
+
 promise.then((result) => {
   const instance = result.instance;
   const module = result.module;
-  console.log('instance:', instance);
-  console.log('instance.exports:', instance.exports);
-  console.log('module:', module);
   const add = instance.exports.add;
   console.log(add(1, 2));
   console.log(instance.exports.addTwo(2, 3));
 });
-
-
-console.log(WebAssembly.Memory);
